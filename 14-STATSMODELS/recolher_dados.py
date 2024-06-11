361226c81c189179f6c652fa0df4809f61e23bf5
Aluno <profissional.guilhermevitor@gmail.com>
2024-06-11 12:55:20 -0300

    Alt1

---
+++ 14-STATSMODELS/recolher_dados.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+file = pd.read_csv('14-STATSMODELS/dataset.csv')
+df = pd.DataFrame(file)
+print(df.columns)
+print(df.info())
